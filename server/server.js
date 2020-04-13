ec3b544bff0469967d155e2ce192731e6dde32e3
Gabe Coelho <gabrielpalmacoelho@gmail.com>
2020-04-12 23:39:22 -0600

    added client .env to docker-compose file and added env vars to server

--- server/server.js
+++ server/server.js
@@ -1,41 +1,41 @@
 const express = require('express')
-const Enforcer = require('openapi-enforcer-middleware')
-const path = require('path')
 const usersController = require('./controllers/users')
 const diapersController = require('./controllers/diapers')
 const feedingsController = require('./controllers/feedings')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
-const LocalStrategy = require('passport-local').Strategy
 const passport = require('passport')
 const session = require('express-session')
-const cors = require('cors')
 const app = express()
-const port = 8081
+const port = process.env.SERVER_PORT
 const User = require('./models/user')
-var MongoDBStore = require('connect-mongodb-session')(session);
+const LocalStrategy = require('passport-local').Strategy
+const MongoDBStore = require('connect-mongodb-session')(session)
+const cors = require('cors')
+require('dotenv').config
 
-var corsOptions = {
-    origin: 'http://localhost:8080',
+const corsOptions = {
+    origin: `${process.env.CLIENT_PROTOCOL}://${process.env.CLIENT_HOST}:${process.env.CLIENT_PORT}`,
     credentials: true
 }
 
 app.use(cors(corsOptions))
 
 const connectWithRetry = () => {
-    mongoose.connect('mongodb://mongo:27017/babeed', { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
-        console.log('MongoDB connection successful')
-    }).catch(err => {
-        console.log('MongoDB connection unsuccessful, retry after 5 seconds... ' + err)
-        setTimeout(connectWithRetry, 5000)
-    })
+    mongoose.connect(`${process.env.DB_PROTOCOL}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+        { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
+            console.log('MongoDB connection successful')
+        }).catch(err => {
+            console.log('MongoDB connection unsuccessful, retrying after 5 seconds... ' + err)
+            setTimeout(connectWithRetry, 5000)
+        })
 }
 
 connectWithRetry()
 
-var store = new MongoDBStore({
-    uri: 'mongodb://mongo:27017/babeed',
-    collection: 'sessions'
+const store = new MongoDBStore({
+    uri: `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    collection: process.env.SESSION_DB_COLLECTION_NAME
 });
 
 passport.use(new LocalStrategy({
@@ -71,9 +71,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(session({
-    secret: "in pheenax not family the",
+    secret: process.env.SESSION_SECRET,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+        // 1 week
+        maxAge: 1000 * 60 * 60 * 24 * 7,
         secure: false
     },
     store: store, resave: true, saveUninitialized: true
@@ -81,47 +82,11 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
-const controllerDirectory = path.resolve(__dirname, 'controllers')
-const mockDirectory = path.resolve(__dirname, 'mock-controllers')
-const pathToOpenApiDoc = path.resolve(__dirname, 'babeed_api.yaml')
-
-const enforcer = Enforcer(pathToOpenApiDoc)
-
-// Task 2 - Middleware that all requests go through
 app.use((req, res, next) => {
     console.log("Path: " + req.path)
     console.log("Method: " + req.method)
     next()
 })
-
-// Handle all mock requests
-// enforcer.mocks(mockDirectory, false)
-//     .catch(console.error)
-
-// // Handle all real requests
-// enforcer.controllers(controllerDirectory)
-//     .catch(console.error)
-
-// // Fallback handler if the controller does not provide a response
-// enforcer.mocks(mockDirectory, true)
-//     .catch((err) => { console.log(err) })
-
-// // User enforcer middleware
-// app.use(enforcer.middleware())
-
-// // Error handling middleware
-// app.use((err, req, res, next) => {
-//     // If the error was in the client's request then send back a detailed report
-//     if (err.statusCode >= 400 && err.statusCode < 500 && err.exception) {
-//         res.set('Content-Type', 'text/plain')
-//         res.status(err.statusCode)
-//         res.send(err.message)
-//     } else {
-//         console.error(err.stack)
-//         res.sendStatus(err.statusCode || 500)
-//     }
-// })
-
 
 // HTTP Handlers
 // Users
@@ -136,14 +101,6 @@ app.put('/users', passport.authenticate('local'), async (req, res) => {
 app.delete('/users', usersController.logout)
 
 // Diapers
-// app.get('/diapers/:id', (req, res) => {
-//     if (!req.user) return res.sendStatus(401);
-//     diapersController.getDiaperById(req, res)
-// })
-// app.get('/diapers', (req, res) => {
-//     if (!req.user) return res.sendStatus(401);
-//     diapersController.getDiapers(req, res)
-// })
 app.get('/diapers/:username', (req, res) => {
     if (!req.user) return res.sendStatus(401);
     diapersController.getDiapersByUser(req, res)
@@ -162,14 +119,6 @@ app.delete('/diapers/:id', (req, res) => {
 })
 
 // Feedings
-// app.get('/feedings/:id', (req, res) => {
-//     if (!req.user) return res.sendStatus(401);
-//     feedingsController.getFeedingById(req, res)
-// })
-// app.get('/feedings', (req, res) => {
-//     if (!req.user) return res.sendStatus(401);
-//     feedingsController.getFeedings(req, res)
-// })
 app.get('/feedings/:username', (req, res) => {
     if (!req.user) return res.sendStatus(401);
     feedingsController.getFeedingsByUser(req, res)
